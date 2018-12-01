cfd0e852552f55a7e067e43ead98bb269298a2f8
Zimo <peter.zimon@gmail.com>
2018-12-01 15:09:20 +0100

    Testing FB comments

--- src/components/header.js
+++ src/components/header.js
@@ -11,18 +11,6 @@ class Header extends React.Component {
 
         return (
             <>
-                <div dangerouslySetInnerHTML={ {
-                    __html: `
-                        <div id="fb-root"></div>
-                        <script>(function(d, s, id) {
-                        var js, fjs = d.getElementsByTagName(s)[0];
-                        if (d.getElementById(id)) return;
-                        js = d.createElement(s); js.id = id;
-                        js.src = 'https://connect.facebook.net/hu_HU/sdk.js#xfbml=1&version=v3.2';
-                        fjs.parentNode.insertBefore(js, fjs);
-                        }(document, 'script', 'facebook-jssdk'));</script>
-                    `
-                } } />
                 <header className={ Styles.page.xl + Styles.sideSpacing + `flex flex-wrap-ns justify-between items-center mt4` }>
 
                     <Link to="/" className="dn db-ns link dim order-1 order-2-l mb4 mb0-l"><Icon name="home" class="dark-gray" /></Link>
