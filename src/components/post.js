a88388796b91c9f8bf0aa2818c5ece72c442c67d
Zimo <peter.zimon@gmail.com>
2018-12-02 00:10:24 +0100

    Testing FB comments

--- src/components/post.js
+++ src/components/post.js
@@ -46,13 +46,30 @@ class Post extends React.Component {
                     <div className="lh-copy serif f4">{ body }</div>
                 </div>
 
-                {(meta ? <div className={ Styles.page.m + Styles.sideSpacing + `post-container mt5 pb4` }>
+                {/* {(meta ? <div className={ Styles.page.m + Styles.sideSpacing + `post-container mt5 pb4` }>
                     <h2 className="f2 bw3 bt b--gold">Beszélgetés</h2>
 
                     <FacebookProvider language="hu_HU" appId={ meta.facebook.appId }>
                         <Comments href={ `https://hellobudaors.com/${slug}` } width="100%" colorscheme="light" />
                     </FacebookProvider>
-                </div> : null)}
+                </div> : null)} */}
+
+
+                <div dangerouslySetInnerHTML={{
+                    __html: `
+                    <div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/hu_HU/sdk.js#xfbml=1&version=v3.2&appId=2081011241955840';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+                    `
+                }} />
+
+                <div class="fb-comments" data-href={`https://hellobudaors.com${slug}`} data-numposts="5"></div>
+
             </div>
         )
     }
