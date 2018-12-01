31550b4488eb9462039260ccdb44c72c96b2cba7
Zimo <peter.zimon@gmail.com>
2018-12-01 11:20:24 +0100

    Add some basic metadata

--- src/components/layout.js
+++ src/components/layout.js
@@ -15,8 +15,22 @@ class DefaultLayout extends React.Component {
                 <Helmet>
                     <title>Hello Budaörs</title>
                     <link href="https://fonts.googleapis.com/css?family=Barlow+Condensed:500|Barlow:700" rel="stylesheet"></link>
-                    {/* <script type="text/javascript" src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script> */}
-                    {/* <script type="text/javascript" src="../assets/js/custom.js"></script> */}
+                    <meta name="description" content="Hello Budaörs! Ismerjük meg városunkat együtt!" />
+                    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+                    <link rel="canonical" href="https://hellobudaors.ghost.io/" />
+                    <meta name="referrer" content="no-referrer-when-downgrade" />
+                    <link rel="next" href="https://hellobudaors.ghost.io/page/2/" />
+
+                    <meta property="og:site_name" content="Hello Budaörs" />
+                    <meta property="og:type" content="website" />
+                    <meta property="og:title" content="Hello Budaörs" />
+                    <meta property="og:description" content="Hello Budaörs! Ismerjük meg városunkat együtt!" />
+                    <meta property="og:url" content="https://hellobudaors.com/" />
+                    <meta name="twitter:card" content="summary" />
+                    <meta name="twitter:title" content="Hello Budaörs" />
+                    <meta name="twitter:description" content="Hello Budaörs! Ismerjük meg városunkat együtt!" />
+                    <meta name="twitter:url" content="https://hellobudaors.com/" />
+                    
                     <body className="sans-serif dark-gray bg-white" />
                 </Helmet>
 
